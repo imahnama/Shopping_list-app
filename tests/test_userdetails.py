18cfa835d9bc3c57aa16fa0a78968c33ac6aabac
imahnama <muannama25@gmail.com>
2017-09-23 10:14:29 +0300

    (chore) add test for username and password

--- tests/test_userdetails.py
+++ tests/test_userdetails.py
@@ -10,6 +10,13 @@ class TestUser(unittest.TestCase):
         """Destroy the object after every test"""
         del self.user
 
+    def test_user_created(self):
+        self.assertIsInstance(self.user, UserDetails)
+
+    def test_signup(self):
+        self.user.signup('nama','muannama25@gmail.com','1234')
+        username = self.user.usernameself.assertIs(username,'nama')
+
 if __name__ == '__main__':
     """run the tests"""
     unittest.main()
